f68f268de79fb735f058d45ea2d10dd221d31246
lauraWick <laura@maurowick.com>
2026-02-22 19:24:11 -0300

    adicionando rotas

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\CategoriaController;
+
 
 Route::get('/', [ProjectController::class, 'index']);
 Route::get('/about', [ProjectController::class, 'about']);
+Route::get('/categorias', [CategoriaController::class, 'index']);
 
 Route::get('/teste', function () {
     return view('teste');
